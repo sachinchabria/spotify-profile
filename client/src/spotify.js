76fd117765f35a1b3d21ce7587397fc44c42d4dd
sachhabria <sach.chhabria@gmail.com>
2024-08-16 14:09:41 -0700

    Declare constants at the top of spotify.js file

--- client/src/spotify.js
+++ client/src/spotify.js
@@ -1,8 +1,24 @@
-const getAccessToken = () => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const accessToken = urlParams.get('access_token');
-    return accessToken;
-  };
+// Map for localStorage keys
+const LOCALSTORAGE_KEYS = {
+	accessToken: 'spotify_access_token',
+	refreshToken: 'spotify_refresh_token',
+	expireTime: 'spotify_token_expire_time',
+	timestamp: 'spotify_token_timestamp',
+}
   
-  export const accessToken = getAccessToken();
+// Map to retrieve localStorage values
+const LOCALSTORAGE_VALUES = {
+	accessToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.accessToken),
+	refreshToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.refreshToken),
+	expireTime: window.localStorage.getItem(LOCALSTORAGE_KEYS.expireTime),
+	timestamp: window.localStorage.getItem(LOCALSTORAGE_KEYS.timestamp),
+};
+
+const getAccessToken = () => {
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	const accessToken = urlParams.get('access_token');
+	return accessToken;
+};
+
+export const accessToken = getAccessToken();
