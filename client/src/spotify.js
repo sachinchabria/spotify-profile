4ded8af45e855c459bad5d2722f25d3c13e0dfd2
sachhabria <sach.chhabria@gmail.com>
2024-08-16 14:14:12 -0700

    Add function to logout (remove local storage items)

--- client/src/spotify.js
+++ client/src/spotify.js
@@ -104,4 +104,19 @@ const getAccessToken = () => {
   return false;
 };
 
+
+/**
+ * Clear out all localStorage items we've set and reload the page
+ * @returns {void}
+ */
+export const logout = () => {
+  // Clear all localStorage items
+  for (const property in LOCALSTORAGE_KEYS) {
+    window.localStorage.removeItem(LOCALSTORAGE_KEYS[property]);
+  }
+  // Navigate to homepage
+  window.location = window.location.origin;
+};
+
+
 export const accessToken = getAccessToken();
